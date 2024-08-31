bf9b063c0864a4dd96b2a7193cd809da216665e3
Amanda Martinez <41299476+amartinez62@users.noreply.github.com>
2024-08-31 10:54:18 -0500

    Initial commit

---
+++ cypress.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    baseUrl: "http://localhost:3000/", // update to envvar and set based on environment
+  },
+});
